d364a4e44e0db100910e4c5568391d79bfe560a0
Jan Ulrichts <jan.ulrichts@gmail.com>
2026-02-16 16:31:03 +0100

    Partially implemented Element element.

--- src/XsdBuilding.Xsd/Element.fs
+++ src/XsdBuilding.Xsd/Element.fs
@@ -1,9 +1,9 @@
 [<RequireQualifiedAccess>]
 module Element
 
-type Element<'TAttributes> = {
+type Element<'TAnnotationAppinfoContent, 'TAnnotationDocumentationContent, 'TAnnotationAttributes, 'TAttributes> = {
     ElementAttributes: ElementAttributes<'TAttributes>;
-    ElementContent: ElementContent;
+    ElementContent: ElementContent<'TAnnotationAppinfoContent, 'TAnnotationDocumentationContent, 'TAnnotationAttributes>;
 }
 and ElementAttributes<'TAttributes> = {
     Abstract: Xml.Bool;
@@ -21,23 +21,15 @@ and ElementAttributes<'TAttributes> = {
     Type: Xml.QName;
     AdditionalAttributes: 'TAttributes option
 }
-and ElementContent = ElementContent
-
-// <element
-//   abstract = Boolean : false
-//   block = (#all | List of (extension | restriction | substitution))
-//   default = string
-//   final = (#all | List of (extension | restriction))
-//   fixed = string
-//   form = (qualified | unqualified)
-//   id = ID
-//   maxOccurs = (nonNegativeInteger | unbounded) : 1
-//   minOccurs = nonNegativeInteger : 1
-//   name = NCName
-//   nillable = Boolean : false
-//   ref = QName
-//   substitutionGroup = QName
-//   type = QName
-//   {any attributes with non-schema Namespace}...>
-// Content: (annotation?, ((simpleType | complexType)?, (unique | key | keyref)*))
-// </element>
+and ElementContent<'TAnnotationAppinfoContent, 'TAnnotationDocumentationContent, 'TAnnotationAttributes> = {
+    Annotation: Annotation.Annotation<'TAnnotationAppinfoContent, 'TAnnotationDocumentationContent, 'TAnnotationAttributes> option;
+    TypeCategory: TypeCategory option;
+    ReferenceCategories: ReferenceCategory list
+}
+and TypeCategory =
+    | ComplexType of ComplexType.ComplexType
+    | SimpleType of SimpleType.SimpleType
+and ReferenceCategory =
+    | Key of Key.Key
+    | Keyref of Keyref.Keyref
+    | Unique of Unique.Unique
